de1d26f2dfb88cc701f383bdf0f1f73f4814f936
sean grogan 2 <seangroganwebdevelopment@gmail.com>
2024-07-21 18:18:43 +0200

    24.07.21 6.18pm

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById('hamburger').addEventListener('click', function() {
     document.getElementById('nav-links').classList.toggle('active');
 });
+
+  // JavaScript to hide the text box on scroll and show it when scrolled back to the top
+  window.addEventListener('scroll', function() {
+    const textBox = document.getElementById('hero-text');
+    if (window.scrollY > 0) {
+        textBox.style.opacity = '0';
+        textBox.style.visibility = 'hidden';
+    } else {
+        textBox.style.opacity = '1';
+        textBox.style.visibility = 'visible';
+    }
+});
